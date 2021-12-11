afb05b3f34f8ba5ce392c33a8ca3a0e33deffc8d
Chimq12 <tinhbu0123@gmail.com>
2021-12-11 16:38:50 +0700

    Push MVC

---
+++ CNPM_Tuan7/CNPM_Tuan7/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CNPM_Tuan7.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
